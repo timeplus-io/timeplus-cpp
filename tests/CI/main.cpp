e3611e012cc0880ba118a4f9f1a0f12bb52c2ed7
Jax-YHH <jax.yang@Timeplus.io>
2024-07-30 18:08:59 +0800

    start issue#11 enhancement

---
+++ tests/CI/main.cpp
@@ -0,0 +1,38 @@
+#include "TestFunctions.h"
+
+
+static void RunTests(Client& client) {
+    testIntType(client);
+    testDecimalType(client);
+    testArrayType(client);
+    testDateTimeType(client);
+    testFloatType(client);
+    testUUIDType(client);
+    testStringType(client);
+    testEnumType(client);
+    testIPType(client);
+    testMultitesArrayType(client);
+    testNullabletype(client);
+}
+
+int main()
+{
+    /// Initialize client connection.
+    try {
+        const auto localHostEndpoint = ClientOptions()
+                .SetHost("localhost")
+                .SetPort(8463);
+
+        {
+            Client client(ClientOptions(localHostEndpoint)
+                    .SetPingBeforeQuery(true));
+            RunTests(client);
+        }
+
+    } catch (const std::exception& e) {
+        std::cerr << "exception : " << e.what() << std::endl;
+    }
+
+
+    return 0;
+}
