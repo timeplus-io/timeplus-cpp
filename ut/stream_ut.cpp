bb6f39c72097e5921ec7e25702339503db550e73
Sofiane Soufi <sofiane@nyxl.io>
2026-01-22 12:48:19 -0500

    test: add WireFormat::WriteQuotedString cases

--- ut/stream_ut.cpp
+++ ut/stream_ut.cpp
@@ -3,8 +3,26 @@
 #include <timeplus/base/input.h>
 
 #include <gtest/gtest.h>
+#include <string>
 
 using namespace timeplus;
+
+namespace {
+    std::string roundtripQuotedString(const std::string& value) {
+        Buffer buf;
+        {
+            BufferOutput output(&buf);
+            WireFormat::WriteQuotedString(output, value);
+            output.Flush();
+        }
+        ArrayInput input(buf.data(), buf.size());
+        std::string result;
+        if (!WireFormat::ReadString(input, &result)) {
+            return {};
+        }
+        return result;
+    }
+}
 
 TEST(CodedStreamCase, Varint64) {
     Buffer buf;
@@ -21,4 +39,17 @@ TEST(CodedStreamCase, Varint64) {
         ASSERT_TRUE(WireFormat::ReadVarint64(input, &value));
         ASSERT_EQ(value, 18446744071965638648ULL);
     }
+}
+
+TEST(CodedStreamCase, QuotedStringPlain) {
+    ASSERT_EQ(roundtripQuotedString("hello"), "'hello'");
+}
+
+TEST(CodedStreamCase, QuotedStringSingleQuote) {
+    ASSERT_EQ(roundtripQuotedString("a'b"), "'a\\x27b'");
+}
+
+TEST(CodedStreamCase, QuotedStringNullByte) {
+    const std::string value("a\0b", 3);
+    ASSERT_EQ(roundtripQuotedString(value), "'a\\x00b'");
 }
